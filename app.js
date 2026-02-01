0740b5c678d2e93a53d2e18643d8ea42bfd7365b
Erman Urel <erman.urel@gmail.com>
2026-02-01 18:24:35 +0300

    fix9

--- app.js
+++ app.js
@@ -1075,8 +1075,7 @@ window.openAdDetail = function(id) {
 
     // MODERN DÜZENLEME BUTONU VE GÜVENLİ YERLEŞİM
     const editBtn = document.createElement('button');
-    editBtn.className = 'cyber-submit';
-    editBtn.style.cssText = "margin-bottom: 8px; font-weight: bold;";
+    editBtn.className = 'cyber-submit edit-btn-style';
     editBtn.innerHTML = '<i class="fas fa-edit"></i> BU İLANI DÜZENLE';
     editBtn.onclick = () => window.handleAdEdit(ad);
 
@@ -1088,6 +1087,7 @@ window.openAdDetail = function(id) {
         footer.prepend(editBtn); 
 
         const deleteBtn = document.createElement('button');
+        deleteBtn.className = 'cyber-submit delete-btn-style';
         deleteBtn.id = 'modal-delete-btn-inner';
         deleteBtn.innerHTML = '<i class="fas fa-trash-alt"></i> İlanı Kalıcı Olarak Kaldır';
         deleteBtn.onclick = () => window.deleteAd(ad.id);
